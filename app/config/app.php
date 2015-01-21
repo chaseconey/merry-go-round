0897a5bebdfec620092814285af685bf08d3c545
Chase Coney <chasiepoo@gmail.com>
2015-01-21 00:48:45 -0600

    Update app key

--- app/config/app.php
+++ app/config/app.php
@@ -13,7 +13,7 @@ return array(
 	|
 	*/
 
-	'debug' => false,
+	'debug' => true,
 
 	/*
 	|--------------------------------------------------------------------------
@@ -78,7 +78,7 @@ return array(
 	|
 	*/
 
-	'key' => 'YourSecretKey!!!',
+	'key' => 'pxlsFNi46cx8ZuYudOZYKsKM2ctTBKLP',
 
 	'cipher' => MCRYPT_RIJNDAEL_128,
 
